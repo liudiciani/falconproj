0d2e437b27604c8b93d788d35b6140ce9d46abb7
Andrew Raleigh <andrewsraleigh@gmail.com>
2015-10-16 15:56:03 -0400

    Updating about view

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use(testmw);
 var team = require('./lib/team.js');
 
 app.get('/', (req, res) => {
-  // TODO
+  // Change to something other than default team view.
   var result = team.all();
   if (!result.success) {
     notFound404(req, res);
